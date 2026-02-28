cca1ea16562b65039b75a7ef885dc4f070516c63
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-02-28 22:27:47 +0100

    Add treefmt, devshell

--- flake.nix
+++ flake.nix
@@ -5,6 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    import-tree.url = "github:vic/import-tree";
   };
 
   outputs = {
@@ -13,6 +15,10 @@
     ...
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -20,22 +26,42 @@
         "aarch64-darwin"
       ];
 
-      perSystem = {system, ...}: let
+      perSystem = {
+        system,
+        config,
+        pkgs,
+        ...
+      }: let
         nixvimLib = nixvim.lib.${system};
         nixvim' = nixvim.legacyPackages.${system};
         nixvimModule = {
           inherit system;
-          module = import ./config;
-          extraSpecialArgs = {};
+          module = inputs.import-tree ./config;
+          extraSpecialArgs = {inherit inputs;};
         };
         nvim = nixvim'.makeNixvimWithModule nixvimModule;
       in {
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+          programs.deadnix.enable = true;
+          programs.statix.enable = true;
+        };
+
+        formatter = config.treefmt.build.wrapper;
+
         checks = {
           default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
         };
 
         packages = {
           default = nvim;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            nvim
+          ];
         };
       };
     };
